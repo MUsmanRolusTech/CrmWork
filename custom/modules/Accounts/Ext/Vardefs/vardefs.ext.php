d20d3e6724e0c003aa8404b1585898de77f8f74b
Usman Zaheer <usman.zaheer@rolustech.net>
2022-05-12 12:01:24 +0500

    custom module and relationship

--- custom/modules/Accounts/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Accounts/Ext/Vardefs/vardefs.ext.php
@@ -42,6 +42,19 @@ $dictionary['Account']['fields']['csm_contact_id'] = array(
         'duplicate_merge' => 'disabled',
 );
 
+// created: 2022-05-11 15:17:35
+$dictionary["Account"]["fields"]["accounts_fp_events_1"] = array (
+  'name' => 'accounts_fp_events_1',
+  'type' => 'link',
+  'relationship' => 'accounts_fp_events_1',
+  'source' => 'non-db',
+  'module' => 'FP_events',
+  'bean_name' => 'FP_events',
+  'side' => 'right',
+  'vname' => 'LBL_ACCOUNTS_FP_EVENTS_1_FROM_FP_EVENTS_TITLE',
+);
+
+
  // created: 2022-05-05 16:58:43
 $dictionary['Account']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
 
